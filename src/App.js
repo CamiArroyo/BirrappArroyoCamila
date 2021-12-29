137c72fe0574998f8fdf6983d1d5cc4515434f55
CamilaArroyo <camiarroyo98.1@gmail.com>
2021-12-29 15:24:40 -0300

    Primera subida de archivos

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aquí se desarrollará mi proyecto final "Birrapp", tienda de cervezas.
         </p>
         <a
           className="App-link"
