9e6e15c09ec0365e91525d512c05f01497418c96
CamilaArroyo <camiarroyo98.1@gmail.com>
2022-01-25 14:36:56 -0300

    Desafío clase 6 promesas y map

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -5,6 +5,7 @@ const ItemListContainer = () => {
     return (
         <div style={{marginTop:100}}>
             <h1>NUESTROS PRODUCTOS</h1>
+            <h2>Presentamos a continuación una cartilla con nuestros productos.</h2>
             <ItemList/>
         </div>
     )
