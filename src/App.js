0e4a0b48200b16f6f389e7ef360285ddf8634a68
CamilaArroyo <camiarroyo98.1@gmail.com>
2022-01-25 13:58:24 -0300

    Desafio clase 6

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import { useState } from 'react';
+
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 
-const App = () => {
-
-  const [products, setProducts] = useState([
-    {name:"Cerveza Andes IPA lata 473", brand:"Andes"},
-    {name:"Cerveza Andes Negra lata 473", brand:"Andes"}, 
-    {name:"Cerveza Andes Roja lata 473", brand:"Andes"}
-  ])
+function App() {
 
   return (
     <div className="App">
