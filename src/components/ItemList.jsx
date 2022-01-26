ea551521d0425cc874aa2e99eb630e3066c0d5e5
CamilaArroyo <camiarroyo98.1@gmail.com>
2022-01-26 11:23:09 -0300

    Completo el desafío clase 6

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -4,17 +4,24 @@ import Item from './Item';
 
 const ItemList = () => {
 
-    const [items, setItems] = useState([
-        {name:"Cerveza Andes IPA lata 473", brand:"Andes", description:"Aquí habrá una descripción", unitPrice:148, sixPackPrice:880, stock:10},
-        {name:"Cerveza Andes Negra lata 473", brand:"Andes", description:"Aquí habrá una descripción", unitPrice:170, sixPackPrice:1005, stock:15}, 
-        {name:"Cerveza Andes Roja lata 473", brand:"Andes", description:"Aquí habrá una descripción", unitPrice:170, sixPackPrice:1005, stock:0}
-    ])
+    const arregloProductos = [        
+    {name:"Cerveza Andes IPA lata 473", brand:"Andes", description:"Aquí habrá una descripción", unitPrice:148, sixPackPrice:880, stock:10},
+    {name:"Cerveza Andes Negra lata 473", brand:"Andes", description:"Aquí habrá una descripción", unitPrice:170, sixPackPrice:1005, stock:15}, 
+    {name:"Cerveza Andes Roja lata 473", brand:"Andes", description:"Aquí habrá una descripción", unitPrice:170, sixPackPrice:1005, stock:0}
+    ];
+
+    const [items, setItems] = useState([])
+
+    const promise  = new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            resolve(setItems(arregloProductos))
+        },2000)
+    })
 
     return (
         <div>
             {items.map(item => {
                 return <Item ItemName={item.name} ItemBrand={item.brand} Imagen={item.imagen} Description={item.description} UnitPrice={item.unitPrice} SixPackPrice={item.sixPackPrice} Stock={item.stock}/> 
-
             })}
         </div>
     )
